da7a6243cc7e4aaddcd156a146080ff379d3f222
Jal Thakor <jalsthakor413@gmail.com>
2022-03-29 17:17:11 +0530

    update 1.01

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import './Team.css'
+import "./Team.css";
+import Cards from "./cards";
+import Item from "./item.js";
+import React, { useState } from "react";
+import Carousel from "react-elastic-carousel";
 
-function Team() {
-	return (
-		<div>Team</div>
-	)
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 5 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 3},
+];
+
+function Teams() {
+  return (
+    <view>
+    <div className="our-team">Our Team</div>
+
+    <div className="App">
+      <div className="carousel-wrapper">
+        <Carousel breakPoints={breakPoints}>
+          <Item>
+          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Name1" descri="Lorem"></Cards>
+          </Item>
+          <Item>
+          <Cards></Cards>
+          </Item>
+          <Item>
+          <Cards></Cards>
+          </Item>
+          <Item>
+          <Cards></Cards>
+          </Item>
+          <Item>
+          <Cards></Cards>
+          </Item>
+          <Item>
+          <Cards></Cards>
+          </Item>
+          <Item>
+          <Cards></Cards>
+          </Item>
+        </Carousel>
+      </div>
+    </div>
+    </view>
+  );
 }
 
-export default Team
+export default Teams;
---
+++ src/components/Team/item.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  width: 350px;
+  margin: 25px;  
+  padding 30px;
+`;
