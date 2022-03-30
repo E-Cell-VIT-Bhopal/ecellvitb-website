047b7c28d4dfaa5be61b0de4468d34a8c9805d10
Jal Thakor <jalsthakor413@gmail.com>
2022-03-30 15:05:27 +0530

    Update 1.02

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -20,25 +20,22 @@ function Teams() {
       <div className="carousel-wrapper">
         <Carousel breakPoints={breakPoints}>
           <Item>
-          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Name1" descri="Lorem"></Cards>
+          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Person Name" descri="President"></Cards>
           </Item>
           <Item>
-          <Cards></Cards>
+          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Person Name 2" descri="Vice President"></Cards>
           </Item>
           <Item>
-          <Cards></Cards>
+          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Person Name 3" descri="Co-Ordinator"></Cards>
           </Item>
           <Item>
-          <Cards></Cards>
+          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Person Name 4" descri="Teach Lead"></Cards>
           </Item>
           <Item>
-          <Cards></Cards>
+          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Person Name 5" descri="Design Lead"></Cards>
           </Item>
           <Item>
-          <Cards></Cards>
-          </Item>
-          <Item>
-          <Cards></Cards>
+          <Cards path="https://cdn.discordapp.com/attachments/941554263128150036/958046986446176306/curved_logo.png" head="Person Name 6" descri="Content Lead"></Cards>
           </Item>
         </Carousel>
       </div>
